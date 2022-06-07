b9fe15f97750a10298de620576c67fc73e119fd8
Angel Igareta <angel@igareta.com>
2022-06-07 10:08:08 +0000

    Finish lesson 3

--- lesson3/HelloBlazePreprocessLambda.py
+++ lesson3/HelloBlazePreprocessLambda.py
@@ -5,8 +5,8 @@ import os
 import boto3
 from botocore.exceptions import ClientError
 
-BUCKET_NAME = 'FILL_THIS_IN'
-PREFIX = 'FILL_THIS_IN'
+BUCKET_NAME = 'sagemaker-studio-upalsdlzrg'
+PREFIX = 'lesson3-1'
 
 # Function below unzips the archive to the local directory. 
 
